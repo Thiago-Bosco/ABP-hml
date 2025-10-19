632a586104b7ff261e16788d761064603975f79e
Thiago-Bosco <thiago-carvalho789@outlook.com>
2025-10-18 22:41:29 -0300

    first commit

---
+++ src/AbpSolution1.Web/AbpSolution1WebAutoMapperProfile.cs
@@ -0,0 +1,11 @@
+using AutoMapper;
+
+namespace AbpSolution1.Web;
+
+public class AbpSolution1WebAutoMapperProfile : Profile
+{
+    public AbpSolution1WebAutoMapperProfile()
+    {
+        //Define your object mappings here, for the Web project
+    }
+}
